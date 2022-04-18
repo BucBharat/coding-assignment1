7517c03081097e693b41fef59f67720e454b388b
BucBharat <58068566+BucBharat@users.noreply.github.com>
2022-04-18 17:04:09 +0530

    added components

---
+++ src/components/SideBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-app-bar
+      absolute
+      class="mb-3"
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" fixed left app clipped>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-domain</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>Company Name</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Menu element</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Menu element</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Menu element</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <template v-slot:append>
+        <v-list-item>
+          <v-btn block color="blue" @click="signOut">
+            <v-list-item-icon>
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Admin</v-list-item-title>
+            </v-list-item-content>
+          </v-btn>
+        </v-list-item>
+      </template>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,54 +1,6 @@
 <template>
   <div id="pageLayout">
-    <v-app-bar
-      absolute
-      class="mb-3"
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" fixed left app clipped>
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon>mdi-domain</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>Company Name</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Menu element</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Menu element</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Menu element</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <template v-slot:append>
-        <v-list-item>
-          <v-btn block color="blue" @click="signOut">
-            <v-list-item-icon>
-              <v-icon>mdi-logout</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>Admin</v-list-item-title>
-            </v-list-item-content>
-          </v-btn>
-        </v-list-item>
-      </template>
-    </v-navigation-drawer>
+    <SideBar />
 
     <v-row class="mt-14">
       <v-col
@@ -139,15 +91,15 @@
 
 <script>
 import EventService from '../Services/EventService';
+import SideBar from '../components/SideBar.vue';
 
 export default {
-  components: {},
+  components: {
+    SideBar,
+  },
   data() {
     return {
       users: [],
-      modalDisplay: false,
-      currentUser: null,
-      mobileSideBarVar: false,
       dialog: false,
       drawer: false,
     };
@@ -157,7 +109,6 @@ export default {
       this.dialog = true;
       this.currentUser = user;
     },
-
     signOut() {
       localStorage.removeItem('accessToken');
       this.$router.push({ name: 'Login' });
