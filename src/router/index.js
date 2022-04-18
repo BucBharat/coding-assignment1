90abd1db90b3c2e461935c7ceeb735b28e2e202e
BucBharat <58068566+BucBharat@users.noreply.github.com>
2022-04-18 16:17:43 +0530

    removed home route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Users from '../views/Users.vue';
 
@@ -9,8 +8,8 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Login',
+    component: Login,
   },
   {
     path: '/about',
